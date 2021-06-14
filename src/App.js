6c1f051877ed34e1da1284fcf2278d421bbbec12
mie998 <nishiwaki.kyoto@gmail.com>
2021-06-14 20:21:54 +0900

    resize tinder card

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React, { useState } from 'react'
-import './App.css'
-import Switch from 'react-ios-switch'
+import React, { useState } from "react";
+import "./App.css";
 
-import Advanced from './examples/Advanced'
-import Simple from './examples/Simple'
+import Advanced from "./examples/Advanced";
 
-function App () {
-  const [showAdvanced, setShowAdvanced] = useState(true)
-
-  return (
-    <div className='app'>
-      {showAdvanced ? <Advanced /> : <Simple />}
-      <div className='row'>
-        <p style={{ color: '#fff' }}>Show advanced example</p> <Switch checked={showAdvanced} onChange={setShowAdvanced} />
-      </div>
-    </div>
-  )
+function App() {
+    return (
+        <div className="app">
+            <Advanced />
+            <div className="row"></div>
+        </div>
+    );
 }
 
-export default App
+export default App;
